8ca767b71b01d1698e2399cf1a8422dceb9b72c0
Maxime <javeuch@gmail.com>
2021-04-30 07:27:10 +0200

    Added: Initialisation de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+
+  /* Création d'un constructor avec les informations dans le script de firebase */
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyA1Xg50gtVR6-ZzKblk6be37DPXvrzfwdg",
+      authDomain: "bookshelves-2ff1e.firebaseapp.com",
+      projectId: "bookshelves-2ff1e",
+      storageBucket: "bookshelves-2ff1e.appspot.com",
+      messagingSenderId: "770749707719",
+      appId: "1:770749707719:web:5cfed6cf4cae0af352d5e2",
+      measurementId: "G-41YJ2C3138"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+   }
 }
---
+++ src/app/models/book.model.ts
@@ -0,0 +1,6 @@
+export class Book {
+    photo!: string; //! Point exclamation
+    constructor(public title:string, public author:string) {
+
+    }
+}
